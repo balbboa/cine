ea6545d1af1779944ce0493528919640d0bb93be
balboa_cs <cs3@cloudplusplus.nl>
2025-03-31 16:43:45 -0300

    Replace @ts-ignore with @ts-expect-error to satisfy ESLint rules

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -33,7 +33,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
       <DialogContent className="overflow-hidden p-0 shadow-lg">
         <DialogTitle className="sr-only">Search</DialogTitle>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-gray-500 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5 dark:[&_[cmdk-group-heading]]:text-gray-400">
-          {/* @ts-ignore */}
+          {/* @ts-expect-error - Type conflict between React's ReactNode and cmdk's expected node type */}
           {children}
         </Command>
       </DialogContent>
